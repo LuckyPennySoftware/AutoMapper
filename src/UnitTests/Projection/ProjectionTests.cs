239a87b951fbd6abf1c69c56a509daa43468b5ae
Jimmy Bogard <jimmy.bogard@gmail.com>
2025-12-30 10:10:16 -0600

    Removing compile error

--- src/UnitTests/Projection/ProjectionTests.cs
+++ src/UnitTests/Projection/ProjectionTests.cs
@@ -47,7 +47,7 @@ public class ProjectionAndMappingCombined : NonValidatingSpecBase
     {
         var mappingConfiguration = new MapperConfiguration(cfg =>
         {
-            cfg.CreateProjection<Source, Destination>().Advanced().;
+            cfg.CreateProjection<Source, Destination>();
             cfg.CreateMap<Source, Destination>();
         });
         
